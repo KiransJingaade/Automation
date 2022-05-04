98e97da2c9a23fe2c0017bfb606a0391a8359339
kiran <kiran.s7903@gmail.com>
2022-05-04 16:22:19 +0530

    second commit done

---
+++ com.vtiger/src/test/java/crm/TC020Test.java
@@ -0,0 +1,18 @@
+package crm;
+
+
+public class TC020Test {
+	
+public void createProject() {
+	System.out.println("running create project test case");
+}
+
+public void deleteProject() {
+	System.out.println("running delete project test case");
+}
+public static void main(String[]args)
+{
+	
+	}
+
+}
